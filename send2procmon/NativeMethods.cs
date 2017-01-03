e21f91311bd8b9bd1d1f634ab27ab389a9a8533d
Sebastian Solnica <ssolnica@gmail.com>
2017-01-03 18:09:03 +0100

    initial commit

---
+++ send2procmon/NativeMethods.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+
+namespace LowLevelDesign.Send2Procmon
+{
+    class NativeMethods
+    {
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern SafeFileHandle CreateFile(string fileName, 
+            [MarshalAs(UnmanagedType.U4)] FileAccess fileAccess, 
+            [MarshalAs(UnmanagedType.U4)] FileShare fileShare, 
+            IntPtr securityAttributes, 
+            [MarshalAs(UnmanagedType.U4)] CreationDisposition creationDisposition, 
+            [MarshalAs(UnmanagedType.U4)] FileAttributes flags, 
+            IntPtr template);
+
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool DeviceIoControl(SafeFileHandle hDevice, 
+            uint IoControlCode, 
+            [MarshalAs(UnmanagedType.AsAny)] [In] object InBuffer, 
+            uint nInBufferSize, 
+            [MarshalAs(UnmanagedType.AsAny)] [Out] object OutBuffer, 
+            uint nOutBufferSize, 
+            ref uint pBytesReturned, 
+            [In] IntPtr Overlapped);
+
+        [Flags]
+        public enum FileAccess : uint
+        {
+            Read = 2147483648u,
+            Write = 1073741824u,
+            Execute = 536870912u,
+            All = 268435456u
+        }
+
+        [Flags]
+        public enum FileShare : uint
+        {
+            None = 0u,
+            Read = 1u,
+            Write = 2u,
+            Delete = 4u
+        }
+
+        public enum CreationDisposition : uint
+        {
+            New = 1u,
+            CreateAlways,
+            OpenExisting,
+            OpenAlways,
+            TruncateExisting
+        }
+
+        [Flags]
+        public enum FileAttributes : uint
+        {
+            Readonly = 1u,
+            Hidden = 2u,
+            System = 4u,
+            Directory = 16u,
+            Archive = 32u,
+            Device = 64u,
+            Normal = 128u,
+            Temporary = 256u,
+            SparseFile = 512u,
+            ReparsePoint = 1024u,
+            Compressed = 2048u,
+            Offline = 4096u,
+            NotContentIndexed = 8192u,
+            Encrypted = 16384u,
+            Write_Through = 2147483648u,
+            Overlapped = 1073741824u,
+            NoBuffering = 536870912u,
+            RandomAccess = 268435456u,
+            SequentialScan = 134217728u,
+            DeleteOnClose = 67108864u,
+            BackupSemantics = 33554432u,
+            PosixSemantics = 16777216u,
+            OpenReparsePoint = 2097152u,
+            OpenNoRecall = 1048576u,
+            FirstPipeInstance = 524288u
+        }
+    }
+}
---
+++ send2procmon/Program.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace LowLevelDesign.Send2Procmon
+{
+    static class Program
+    {
+        private const uint IoCtlCode = 2503311876;
+        private const int MaxSingleMessageLength = 2047;
+
+        static void Main(string[] args)
+        {
+            if (ShouldIPrintHelpAndExit(args)) {
+                PrintHelp();                
+                return;
+            }
+
+            string[] messages = args;
+            if (args.Length == 0) {
+                if (!Console.KeyAvailable) {
+                    return;
+                }
+                var l = new List<string>();
+                string msg;
+                while ((msg = Console.ReadLine()) != null) {
+                    l.Add(msg);
+                }
+                messages = l.ToArray();
+            }
+            var hProcmonDevice = NativeMethods.CreateFile(
+                @"\\.\Global\ProcmonDebugLogger", NativeMethods.FileAccess.Write | NativeMethods.FileAccess.Read,
+                NativeMethods.FileShare.Write, IntPtr.Zero, NativeMethods.CreationDisposition.OpenExisting,
+                NativeMethods.FileAttributes.Normal, IntPtr.Zero);
+            if (hProcmonDevice.IsInvalid || hProcmonDevice.IsClosed) {
+                int err = Marshal.GetLastWin32Error();
+                Console.Error.WriteLine("Can't connect to procmon device: 0x{0:X}. Probably procmon is not running.",
+                    err);
+                return;
+            }
+            try {
+                foreach (var message in messages) {
+                    SplitMessagesIfNecessaryAndSendThemToProcmon(hProcmonDevice, message);
+                }
+            }
+            finally {
+                hProcmonDevice.Dispose();
+            }
+        }
+
+        static void SplitMessagesIfNecessaryAndSendThemToProcmon(SafeFileHandle hProcmonDevice, string message)
+        {
+            if (message.Length <= MaxSingleMessageLength) {
+                SendOneMessageToProcmon(hProcmonDevice, message);
+                return;
+            }
+
+            var activityId = $"[{Guid.NewGuid():D}]: ";
+            int offset = 0;
+            while (offset < message.Length) {
+                int length = Math.Min(MaxSingleMessageLength - activityId.Length, message.Length - offset);
+                var buffer = activityId + message.Substring(offset, length);
+                offset += length;
+                SendOneMessageToProcmon(hProcmonDevice, buffer);
+            }
+        }
+
+        static void SendOneMessageToProcmon(SafeFileHandle hProcmonDevice, string message)
+        {
+            Debug.Assert(message.Length <= MaxSingleMessageLength);
+            uint bytesReturned = 0;
+            if (!NativeMethods.DeviceIoControl(hProcmonDevice, IoCtlCode, message, (uint)(message.Length*2), null, 0u,
+                ref bytesReturned, IntPtr.Zero)) {
+                int err = Marshal.GetLastWin32Error();
+                if (err == 0x57) {
+                    Console.Error.WriteLine("Either Procmon is not running or tracing is disabled (error code: 0x57).");
+                } else {
+                    Console.Error.WriteLine("Failed to write to procmon device: 0x{0:X}", err);
+                }
+            }
+        }
+
+        static bool ShouldIPrintHelpAndExit(string[] args)
+        {
+            return args.Length > 0 &&
+                   (string.Equals(args[0], "-?") ||
+                    string.Equals(args[0], "--help") ||
+                    string.Equals(args[0], "-help") ||
+                    string.Equals(args[0], "-h"));
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine("send2procmon v{0} - sends input to procmon",
+                Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            Console.WriteLine("Copyright (C) 2017 Sebastian Solnica (@lowleveldesign)");
+            Console.WriteLine();
+            Console.WriteLine("Usage: send2procmon <message-to-send>");
+            Console.WriteLine();
+        }
+    }
+}
